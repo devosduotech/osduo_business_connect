0a0d8a5bddeef34a39a5c5803815cc75777b1035
OSDuo Tech <developer@osduotech.com>
2026-09-06 14:59:34 +0530

    fix: jinja.filters hook must be a list of module paths per Frappe docs

--- osduo_business_connect/hooks.py
+++ osduo_business_connect/hooks.py
@@ -192,10 +192,9 @@ after_migrate = "osduo_business_connect.install.after_migrate"
 # ----------
 
 jinja = {
-    "filters": {
-        "safe_url": "osduo_business_connect.utils.website.safe_url",
-        "safe_video_url": "osduo_business_connect.utils.website.safe_video_url",
-    },
+    "filters": [
+        "osduo_business_connect.utils.website",
+    ],
 }
 
 # Template Overrides
